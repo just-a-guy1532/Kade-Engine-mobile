cb04459f2d0c6b73368397ea77ea4b6d7e41a4ae
Just a guy <103865615+just-a-guy1532@users.noreply.github.com>
2023-03-20 16:16:10 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2504,7 +2504,6 @@ class PlayState extends MusicBeatState
 
 					if (SONG.validScore)
 					{
-						NGio.unlockMedal(60961);
 						Highscore.saveWeekScore(storyWeek, campaignScore, storyDifficulty);
 					}
 
