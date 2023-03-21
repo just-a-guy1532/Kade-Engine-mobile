f8f014b950b94e6f6e20afe537e03c42b61ea190
Just a guy <103865615+just-a-guy1532@users.noreply.github.com>
2023-03-22 00:04:33 +0100

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -47,8 +47,12 @@ class GameOverSubstate extends MusicBeatSubstate
 
 		bf.playAnim('firstDeath');
 		
-		 #if mobileC
+		#if mobileC
 		addVirtualPad(NONE, A_B);
+		var camcontrol = new FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		_virtualpad.cameras = [camcontrol];	
 		#end
 	}
 
