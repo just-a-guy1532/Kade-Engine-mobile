22beb329635db7a90467c6011f84830ff4d7d661
Just a guy <103865615+just-a-guy1532@users.noreply.github.com>
2023-03-22 17:04:46 +0100

    a

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -13,7 +13,7 @@ import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
 import lime.app.Application;
-import flixel.FlxButton; //bruh moment
+import flixel.ui.FlxButton; //bruh moment
 
 #if windows
 import Discord.DiscordClient;
